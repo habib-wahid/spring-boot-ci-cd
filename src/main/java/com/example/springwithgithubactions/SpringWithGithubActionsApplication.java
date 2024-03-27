c93f42d207d9c99d74bdc26df4ec9a30693e8b10
habib_wahid <habib11wahid@gmail.com>
2024-03-28 02:48:58 +0600

    first api completed

---
+++ src/main/java/com/example/springwithgithubactions/SpringWithGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwithgithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithGithubActionsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringWithGithubActionsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/springwithgithubactions/SpringWithGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwithgithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithGithubActionsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
