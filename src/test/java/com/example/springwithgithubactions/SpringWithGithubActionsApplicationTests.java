ff736bec031f3822e0bc0e24812957aad993ead9
habib_wahid <habib11wahid@gmail.com>
2024-04-01 02:01:24 +0600

    first test method added

--- src/test/java/com/example/springwithgithubactions/SpringWithGithubActionsApplicationTests.java
+++ src/test/java/com/example/springwithgithubactions/SpringWithGithubActionsApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.springwithgithubactions;
 
+import com.example.springwithgithubactions.controller.GreetingController;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SpringWithGithubActionsApplicationTests {
 
+  @Autowired
+  private GreetingController greetingController;
+
   @Test
-  void contextLoads() {
+  void testHelloMethod() {
+    assertEquals("hello", greetingController.sayHello());
   }
+
 
 }
